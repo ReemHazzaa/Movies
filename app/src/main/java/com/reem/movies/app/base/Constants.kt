4f6cbe6ca6a2f83554e2b5e96b84ca3c78bea4a1
Reem Hazza3 <reemhazzaa4@gmail.com>
2023-05-20 04:10:14 +0200

    Added some binding adapters

--- app/src/main/java/com/reem/movies/app/base/Constants.kt
+++ app/src/main/java/com/reem/movies/app/base/Constants.kt
@@ -3,4 +3,5 @@ package com.reem.movies.app.base
 object Constants {
     const val TABLE_MOVIES_CACHED = "movies_cache_table"
     const val TABLE_MOVIES_FAV = "favorites_table"
+    const val MOVIE_DB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
 }
---
+++ app/src/main/java/com/reem/movies/app/binding/ImageBindingAdapter.kt
@@ -0,0 +1,29 @@
+package com.reem.movies.app.binding
+
+import android.util.Log
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.reem.movies.R
+import com.reem.movies.app.base.Constants.MOVIE_DB_IMAGE_BASE_URL
+
+object ImageBindingAdapter {
+
+    @JvmStatic
+    @BindingAdapter("loadImageWithGlide")
+    fun ImageView.loadImageFromMovieDBWithGlide(url: String?) {
+        try {
+            Glide
+                .with(this.rootView)
+                .load("$MOVIE_DB_IMAGE_BASE_URL$url")
+//                .centerCrop()
+                .error(R.drawable.baseline_error_outline_24)
+                .placeholder(R.drawable.baseline_downloading_24)
+                .into(this)
+
+        } catch (e: java.lang.Exception) {
+            Log.e("Glide", e.toString())
+        }
+    }
+
+}
